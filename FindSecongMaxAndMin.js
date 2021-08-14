30d205582075f906d61d3d55351ffba65ef28e73
Jebakani <vjishwarya@gmail.com>
2021-08-14 09:32:29 +0530

    [JEBAKANI]Add: Storing the generated element in array

--- FindSecongMaxAndMin.js
+++ FindSecongMaxAndMin.js
@@ -1,7 +1,10 @@
 console.log("Implementing the array");
 //p1-A-Generating 10 random number
+//p1-B Storing in array
+let randomArray=new Array();
 for(let i=0;i<10;i++)
 {
     let num=Math.floor(Math.random()*900)+100;
-    console.log(num);
+    randomArray.push(num);
 }
+console.log(randomArray);
