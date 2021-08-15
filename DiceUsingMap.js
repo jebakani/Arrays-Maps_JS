231e20dbd78f5df4b52a7fe2e6cbfb73a2ee74c4
Jebakani <vjishwarya@gmail.com>
2021-08-15 21:46:49 +0530

    [JEBAKANI] Refactor: generate the random dice number till any one number reached the count of 10

--- DiceUsingMap.js
+++ DiceUsingMap.js
@@ -1,3 +1,34 @@
 //a.Generate the random dice number
-let dice=Math.floor(Math.random()*6)+1;
-console.log(dice);
+//b,c repeate and store the result in dictionary
+//check whether the count is 10
+function CheckCount(num)
+{
+    if(num==10)
+    {
+        return false;
+    }
+    return true;
+}
+let diceMap=new Map();
+let count=1;
+while(CheckCount(count))
+{
+    //generate the random number
+    let dice=Math.floor(Math.random()*6)+1;
+    //if the key is already present
+    if(diceMap.has(dice))
+    {
+        //get the value of the key and add count
+       count= diceMap.get(dice)+1;
+       //set the dice
+       diceMap.set(dice,count);
+    }
+    else
+    {
+        //else create the new key with initial count as 1;
+        count=1;
+        diceMap.set(dice,count);
+        
+    }
+}
+console.log(diceMap);
