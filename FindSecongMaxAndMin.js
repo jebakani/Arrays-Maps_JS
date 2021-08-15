16ed66748b529dd51bdf781843b5c5e47275e209
Jebakani <vjishwarya@gmail.com>
2021-08-15 18:59:38 +0530

    [JEBAKANI] Add: Finding second min and second max without using sorting

--- FindSecongMaxAndMin.js
+++ FindSecongMaxAndMin.js
@@ -7,4 +7,44 @@ for(let i=0;i<10;i++)
     let num=Math.floor(Math.random()*900)+100;
     randomArray.push(num);
 }
+
+//function to get second maximum value 
+function GetSecondMax(num)
+{
+  if(num>max)
+  {
+      secondMax=max;
+      max=num;
+      
+  }   
+  else if(num>secondMax)
+  {
+      secondMax=num;
+  }
+}
+//function to get second minimum value 
+function GetSecondMin(num)
+{
+  if(num<min)
+  {
+      secondMin=min;
+      min=num;
+      
+  }   
+  else if(num<secondMin)
+  {
+      secondMin=num;
+  }
+}
+//assign the value of first and second element of array as min and max and second min and second max
+let max=randomArray[0];
+let secondMax=randomArray[1];
+let min=randomArray[0];
+let secondMin=randomArray[1];
 console.log(randomArray);
+randomArray.forEach(GetSecondMax);
+randomArray.forEach(GetSecondMin);
+console.log('Second maximum is :'+secondMax);
+console.log('Second minimum is :'+secondMin);
+
+
